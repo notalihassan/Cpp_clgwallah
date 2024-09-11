c65c7a316aaa79354da1dfd1989bec12964179f3
ali hassan <alihassan92199@gmail.com>
2024-09-11 22:47:33 +0530

    adding files

---
+++ loops/basics/basicForLoop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // FOR LOOP Structure :-
+
+    // for( int i = 0; i < 10; i++){
+    //     code
+    // }
+
+    //     (int i) means initalixation and declaration
+    //     (i < 10) means condition
+    //     (i++) means increament or decreament
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Hello World!" << endl;
+    }
+}
---
+++ loops/practiceQuestions/printWordUsingInput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter number : ";
+    cin >> n;
+
+    // print n times Hello world!
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Hello World!" << endl;
+    }
+}
